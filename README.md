b385d2f2b5c8679eb0650b95d2f2d07ff216cf5a
Alexandro Blanco <ti3r.bubblenet@gmail.com>
2016-05-16 11:17:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+### PyPi Test ###
+
+This project is a simple test to learn how to create and distribute python packages using PyPi (python package index).
+This project can be installed using tools like pip or easy_install.
+
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'alex_pypi_test',
+    packages = ['alex_pypi_test'],
+    version = '0.1',
+    description = 'Test library for pypi with random utileries',
+    author = 'Alexandro Blanco',
+    author_email= 'ti3r.bubblenet@gmail.com',
+    url = 'https://github.com/ti3r/alex_pypi_test',
+    download_url= '',
+    keywords= ['testing','pypi', 'example'],
+    classifiers = [],
+)
